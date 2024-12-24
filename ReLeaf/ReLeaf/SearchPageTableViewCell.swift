89bb3951322e12072952640825add2e45419971d
Bader2554 <badermoazzam73@gmail.com>
2024-12-25 02:30:46 +0300

    update search page

---
+++ ReLeaf/ReLeaf/SearchPageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchPageTableViewCell.swift
+//  ReLeaf
+//
+//  Created by Bader on 24/12/2024.
+//
+
+import UIKit
+
+class SearchPageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

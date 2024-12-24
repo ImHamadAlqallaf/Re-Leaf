2112723db5f974e1c15e6dac671a43b55dc85816
Bader2554 <badermoazzam73@gmail.com>
2024-12-25 02:34:24 +0300

    new

--- ReLeaf/ReLeaf/SearchPage.swift
+++ ReLeaf/ReLeaf/SearchPage.swift
@@ -7,23 +7,79 @@
 
 import UIKit
 
-class SearchPage: UIViewController {
+class SearchPage: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+   // func updateSearchResults(for searchController: UISearchController) {
+  //      <#code#>
+//    }
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Label1.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? SearchPageTableViewCell
+        cell?.ProductImage.image = UIImage(named: ProductImage[indexPath.row])
 
-        // Do any additional setup after loading the view.
+        cell?.Label1.text = Label1[indexPath.row]
+        cell?.Label2.text = Label2[indexPath.row]
+        return cell!
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var searchtableview: UITableView!
+    @IBOutlet weak var Searchbar: UISearchBar!
+    
+    
+    var Label1 = ["Bamboo ToothBrush", "Reusable water bottle ", "bamboo straws"]
+    
+    var ProductImage = ["image1", "image2", "image3", "image4"]
+    
+    var Label2 = ["10 bd", "12 bd", "13 bd", "10 bd"]
+    
+    var filterData = [String]()
+    var searchbarController: UISearchController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        filterData = Label1
+        searchbarController = UISearchController (searchResultsController: nil)
+        searchbarController.searchBar.delegate = self
+        searchbarController.searchBar.placeholder = "Search here..."
+        searchbarController.obscuresBackgroundDuringPresentation = false
+        self.Searchbar.delegate = self
+        self.searchtableview.delegate = self
+        self.searchtableview.dataSource = self
+        
+      //  tableView.tableHeaderView = searchbarController.searchBar
+   
     }
-    */
+    
 
-}
+    }
+    
+extension SearchPage {
+    
+    func searchResultUpdate (for searchbarcontroller: UISearchController){
+        
+        guard let textField = searchbarcontroller.searchBar.text
+        else {return}
+        
+        
+        if textField.isEmpty {
+            filterData = Label1
+            
+        }
+        else {
+            
+            filterData = Label1.filter{ $0.lowercased().contains(textField.lowercased()) }
+        
+                
+            }
+        }
+        
+    }
+
+
+
--- ReLeaf/ReLeaf/SearchPageTableViewCell.swift
+++ ReLeaf/ReLeaf/SearchPageTableViewCell.swift
@@ -9,15 +9,26 @@ import UIKit
 
 class SearchPageTableViewCell: UITableViewCell {
 
+
+   
+    
+    @IBOutlet weak var ProductImage: UIImageView!
+    
+
+    @IBOutlet weak var Label1: UILabel!
+    
+    
+
+    
+    @IBOutlet weak var Label2: UILabel!
+    
+    
+    @IBOutlet weak var Button1: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }

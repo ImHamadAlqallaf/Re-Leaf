e2515c75170714aa02b8e043b9d22463ff468f95
Bader2554 <badermoazzam73@gmail.com>
2024-12-28 00:40:50 +0300

    new

---
+++ ReLeaf/ReLeaf/dispagecell.swift
@@ -0,0 +1,23 @@
+//
+//  dispagecell.swift
+//  ReLeaf
+//
+//  Created by Bader on 27/12/2024.
+//
+
+import UIKit
+
+class dispagecell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

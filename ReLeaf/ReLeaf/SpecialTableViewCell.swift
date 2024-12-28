6d2d079da9e4cbcb4b3b5176c298b83edfc1d47a
Bader2554 <badermoazzam73@gmail.com>
2024-12-28 10:08:41 +0300

    updated

---
+++ ReLeaf/ReLeaf/SpecialTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SpecialTableViewCell.swift
+//  ReLeaf
+//
+//  Created by Bader on 28/12/2024.
+//
+
+import UIKit
+
+class SpecialTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

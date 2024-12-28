091d4d5ead696ee849e16a41a7b2dee39d9db606
isa <202201276@github.com>
2024-12-28 07:28:32 +0300

    cart page item!!!

---
+++ ReLeaf/ReLeaf/CartItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  CartItemCell.swift
+//  ReLeaf
+//
+//  Created by BP-36-224-10 on 28/12/2024.
+//
+
+import UIKit
+
+class CartItemCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+    
+    
+}
+
+
--- ReLeaf/ReLeaf/CartViewController.swift
+++ ReLeaf/ReLeaf/CartViewController.swift
@@ -17,83 +17,80 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     @IBOutlet weak var tableView: UITableView!
     
+    
     var localData: LocalData!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
-
-        guard tableView != nil else {
-            fatalError("TableView outlet is not connected")
-        }
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-
-        loadLocalData()
-        
-
-        guard localData != nil else {
-            fatalError("localData is not initialized")
-        }
-    }
-
-    func loadLocalData() {
-   
-        let sampleProducts = [
-            Product(id: "product1", name: "Bamboo Toothbrush", price: 3.99, image: "bamboo_toothbrush.png"),
-            Product(id: "product2", name: "Reusable Water Bottle", price: 10.99, image: "reusable_bottle.png")
-        ]
-        
-        let sampleCartItems = [
-            CartItem(productId: "product1", quantity: 2),
-            CartItem(productId: "product2", quantity: 1)
-        ]
-        
-        let sampleShops = [
-            Shop(id: "shop1", name: "EcoMart", products: sampleProducts, cartItems: sampleCartItems)
-        ]
-        
-        localData = LocalData(shops: sampleShops, cartItems: sampleCartItems)
-        
-   
-        tableView.reloadData()
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return localData.cartItems.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CartItemCell", for: indexPath)
-        
-        let cartItem = localData.cartItems[indexPath.row]
-        let product = getProductById(cartItem.productId)
-        
-        cell.textLabel?.text = product.name
-        cell.detailTextLabel?.text = "Price: \(product.price) x \(cartItem.quantity)"
-        
-        return cell
-    }
-    
-    func getProductById(_ id: String) -> Product {
-        for shop in localData.shops {
-            if let product = shop.products.first(where: { $0.id == id }) {
-                return product
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            guard tableView != nil else {
+                fatalError("TableView outlet is not connected")
+            }
+            
+            tableView.delegate = self
+            tableView.dataSource = self
+            
+            loadLocalData()
+            
+            guard localData != nil else {
+                fatalError("localData is not initialized")
             }
         }
-        return Product(id: "", name: "Unknown", price: 0.0, image: "")
-    }
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            localData.cartItems.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .automatic)
+
+        func loadLocalData() {
+            let sampleProducts = [
+                Product(id: "product1", name: "Bamboo Toothbrush", price: 3.99, image: "bamboo_toothbrush.png"),
+                Product(id: "product2", name: "Reusable Water Bottle", price: 10.99, image: "reusable_bottle.png")
+            ]
+            
+            let sampleCartItems = [
+                CartItem(productId: "product1", quantity: 2),
+                CartItem(productId: "product2", quantity: 1)
+            ]
+            
+            let sampleShops = [
+                Shop(id: "shop1", name: "EcoMart", products: sampleProducts, cartItems: sampleCartItems)
+            ]
+            
+            localData = LocalData(shops: sampleShops, cartItems: sampleCartItems)
+            
+            tableView.reloadData()
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return localData.cartItems.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CartItemCell", for: indexPath) as! CartItemCell
+            
+            let cartItem = localData.cartItems[indexPath.row]
+            let product = getProductById(cartItem.productId)
+            
+            cell.nameLabel.text = product.name
+            cell.priceLabel.text = "Price: \(product.price)"
+            cell.quantityLabel.text = "Quantity: \(cartItem.quantity)"
+            
+            return cell
+        }
+        
+        func getProductById(_ id: String) -> Product {
+            for shop in localData.shops {
+                if let product = shop.products.first(where: { $0.id == id }) {
+                    return product
+                }
+            }
+            return Product(id: "", name: "Unknown", price: 0.0, image: "")
+        }
+        
+        func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+            return true
+        }
+        
+        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                localData.cartItems.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
         }
     }
-}

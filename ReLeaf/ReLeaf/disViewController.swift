17f2351ba4bee62dda82ee5f1e39362e1e9d660f
Bader2554 <badermoazzam73@gmail.com>
2024-12-28 03:21:17 +0300

    nn

--- ReLeaf/ReLeaf/disViewController.swift
+++ ReLeaf/ReLeaf/disViewController.swift
@@ -1,29 +1,34 @@
-//
-//  disViewController.swift
-//  ReLeaf
-//
-//  Created by Bader on 27/12/2024.
-//
-
 import UIKit
 
-class disViewController: UIViewController {
-
+class disViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var distableview: UITableView!
+    
+    let discounts = [
+            ("Discount Card 1", "20%", "SFS2"),
+            ("Discount Card 2", "30%", "RET4"),
+            ("Discount Card 3", "50%", "AEG4")
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        distableview.delegate = self
+        distableview.dataSource = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return discounts.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! dispagecell
+                
+                // Configure the cell with data
+                let discount = discounts[indexPath.row]
+                cell.discountcardlabel.text = discount.0
+                cell.percentlabel.text = discount.1
+                cell.discountcodelabel.text = discount.2
+                
+                return cell
     }
-    */
 
 }
--- ReLeaf/ReLeaf/dispagecell.swift
+++ ReLeaf/ReLeaf/dispagecell.swift
@@ -1,14 +1,24 @@
-//
-//  dispagecell.swift
-//  ReLeaf
-//
-//  Created by Bader on 27/12/2024.
-//
-
 import UIKit
 
 class dispagecell: UITableViewCell {
-
+    
+    @IBOutlet weak var discountcardlabel: UILabel!
+    @IBOutlet weak var percentlabel: UILabel!
+    @IBOutlet weak var discountcodelabel: UILabel!
+    
+    @IBAction func Apply(_ sender: Any) {
+        // Create an alert
+        let alert = UIAlertController(title: "Success", message: "Discount has been applied!", preferredStyle: .alert)
+        
+        // Add an OK action to dismiss the alert
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        // Present the alert
+        if let viewController = self.parentViewController() {
+            viewController.present(alert, animated: true, completion: nil)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -16,8 +26,20 @@ class dispagecell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
+}
 
+// Helper extension to find the parent view controller of a cell
+extension UIView {
+    func parentViewController() -> UIViewController? {
+        var responder: UIResponder? = self
+        while let nextResponder = responder?.next {
+            responder = nextResponder
+            if let viewController = responder as? UIViewController {
+                return viewController
+            }
+        }
+        return nil
+    }
 }

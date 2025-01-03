dcfc59540d5759325995f1a2b23c72626353f736
RYVulcan <202201208@student.polytechnic.bh>
2025-01-03 16:45:31 +0300

    Notification finished

--- ReLeaf/ReLeaf/NotificationsTableViewController.swift
+++ ReLeaf/ReLeaf/NotificationsTableViewController.swift
@@ -6,20 +6,23 @@
 //
 
 import UIKit
-import UserNotifications // Import UserNotifications framework
+import UserNotifications
 
-class NotificationsTableViewController: UITableViewController {
+class NotificationsTableViewController: UITableViewController, UNUserNotificationCenterDelegate {
 
     var notifications: [Notification] = []
-
+    var currentNotificationIndex = 0  // To track which notification to show
+    var isNotificationSent = false // To track if the notification was already sent
+    
     @IBOutlet weak var notifbtn: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
-        tableView.rowHeight = 100 // Set default row height
+        tableView.rowHeight = 100
+        showAlert()
         
-        // Request notification authorization
+        // Request notification permission
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
             if granted {
                 print("Notification permission granted.")
@@ -28,19 +31,19 @@ class NotificationsTableViewController: UITableViewController {
             }
         }
         
-        // Show alert when entering the page
-        showAlert()
+        // Set the delegate for notifications
+        UNUserNotificationCenter.current().delegate = self
+        
+        // Load initial notifications (if any)
+        loadNotifications()
     }
 
     func loadNotifications() {
         if let path = Bundle.main.path(forResource: "localData", ofType: "json") {
             do {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path))
-                print("JSON Data: \(String(data: data, encoding: .utf8) ?? "No data")") // Debugging
                 let decodedData = try JSONDecoder().decode(LocalData.self, from: data)
                 notifications = decodedData.notifications
-                print("Notifications loaded: \(notifications)") // Debugging
-                tableView.reloadData()
             } catch {
                 print("Error loading JSON data: \(error)")
             }
@@ -50,62 +53,80 @@ class NotificationsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("Number of rows: \(notifications.count)") // Debugging
-        return notifications.count
+        return currentNotificationIndex  // Return the number of rows up to the current notification index
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationCell", for: indexPath) as! NotificationTableViewCell
         let notification = notifications[indexPath.row]
         cell.notificationlbl.text = "\(notification.message)"
-        print("Configuring cell for row \(indexPath.row): \(notification.message)") // Debugging
         return cell
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    // Implement the heightForRowAt method
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100 // Set the height for each row
-    }
-    
-    // Action method for tab bar button
+    // Action method for the bar button
     @IBAction func tabBarItemTapped(_ sender: UIBarButtonItem) {
-        loadNotifications()
-        sendNotification() // Call sendNotification to schedule a notification
-    }
-    
-    // Create and schedule a notification
-    func sendNotification() {
-        let content = UNMutableNotificationContent()
-        content.title = "New Notifications"
-        content.body = "You have new notifications."
-        content.sound = UNNotificationSound.default
-
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        let request = UNNotificationRequest(identifier: "notification.id.01", content: content, trigger: trigger)
-
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("Error adding notification: \(error.localizedDescription)")
-            } else {
-                print("Notification scheduled successfully.")
-                // Debugging: Print the notification details
-                print("Notification content: \(content)")
-                print("Notification trigger: \(trigger)")
-            }
+        if currentNotificationIndex < notifications.count {
+            // Load one new notification (show next one)
+            currentNotificationIndex += 1
+            tableView.reloadData()  // Reload to display the new notification cell
+            
+            // Send the notification with the message from the new cell
+            let message = notifications[currentNotificationIndex - 1].message
+            sendNotification(withMessage: message)
+        } else {
+            // Show an alert when there are no more notifications
+            showNoMoreNotificationsAlert()
         }
     }
     
+    // Create and schedule a notification with the given message
+    func sendNotification(withMessage message: String) {
+        let content = UNMutableNotificationContent()
+        content.title = "New Notification from Releaf"
+        content.body = message  // Set the body to the message
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 2, repeats: false)
+        let request = UNNotificationRequest(identifier: "notification.id.\(currentNotificationIndex)", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error scheduling notification: \(error.localizedDescription)")
+            } else {
+                print("Notification scheduled successfully with message: \(message)")
+            }
+        }
+    }
+
     // Show alert when entering the page
     func showAlert() {
         let alert = UIAlertController(title: "Notification", message: "You have new notifications.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
+
+    // Show alert when there are no more notifications
+    func showNoMoreNotificationsAlert() {
+        let alert = UIAlertController(title: "No More Notifications", message: "You do not have any notifications.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+
+    // Handle notification while the app is in the foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .badge, .sound])
+    }
+
+    // Handle user tapping the notification
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
 }
+
+
+
+
+
 //    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //        // #warning Incomplete implementation, return the number of rows
 //        return 0
